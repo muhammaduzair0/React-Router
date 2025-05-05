1ed7fdf1ce1a00b4c5460da3cd5f42f05dea2d12
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-05-05 15:00:38 +0500

    Added User to index.jsx file

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import About from "./About/About"
 import Contact from "./Contact/Contact";
 import Header from "./Header/Header"
 import Footer from "./Footer/Footer"
+import User from "./User/User";
 
-export {Home, About, Contact, Header, Footer}
+export {Home, About, Contact, Header, Footer, User}
