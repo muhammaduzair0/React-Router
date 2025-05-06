778b1a6f4a5dada652378e811f9709c44fb4bbd1
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-05-06 14:27:11 +0500

    Added UseEffect & UseState Method To Fetch API of Github Followers And Profile Picture

--- src/components/User/Github.jsx
+++ src/components/User/Github.jsx
@@ -1,5 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
+
+function Github() {
+    const data = useLoaderData()
+//   const [data, setData] = useState([]);
+//   useEffect(() => {
+//     fetch("https://api.github.com/users/muhammaduzair0")
+//       .then((res) => res.json())
+//       .then((data) => setData(data));
+//   }, []);
   return (
     <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
       Github followers: {data?.followers}
