f29fd6a29ec4409d19390f11e11d6817c8b4e02a
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-05-06 14:26:33 +0500

    Added Github Followers Component

---
+++ src/components/User/Github.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+  return (
+    <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
+      Github followers: {data?.followers}
+      <img src={data?.avatar_url} alt="Git Picture" width={200} />
+    </div>
+  );
+}
+
+export default Github;
